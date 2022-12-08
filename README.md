c4ab07a3f4fdf431d431aed6096e2dc654d81c8b
Giulia Colombo <giulia.colombo.127@gmail.com>
2022-12-08 22:13:13 +0100

    Ãupdate README

--- README.md
+++ README.md
@@ -3,40 +3,66 @@
 [Click here to see deployed game](http://github.com)
 
 ## Description
-Chihuahua or muffin is a game where the player is displayed with a set of images of faces of chihuahua dogs and muffin.
-Player is given a category (chihuahua or muffin) randomly, and has to click on the corresponding images once the timer starts.
-If player selects all the right images within the time, player wins.
-If player clicks on the image of the wrong category, player loses.
-If player doesn't click on all the right images within the time, player loses.
+- Chihuahua or muffin is a game where the player is shown a randomized set of images of faces of chihuahua dogs and muffin.
+- Player is given a category (chihuahua or muffin) randomly, and has to click on the corresponding images once the timer starts.
+- If player selects all the right images within the time, player wins.
+- If player clicks on the image of the wrong category, player loses.
+- If player doesn't click on all the right images within the time, player loses.
 
 
 ## MVP
-- game has a grid displaying images
-- game picks and displays a category (C or M)
-- game displays a timer 
-- when game finishes, player can restart it
+- Game has 16 different cards (8 chihuahuas, 8 muffins)
+- cards are displayed in the game area in a different, random order at the start of the game
+- game picks and displays a category (chihuahua or muffin)
+- game displays a timer with a countdown
+- game displays game over/game won accordingly
+- game asks player if they want to play again
 
 
 ## Backlog
-- keep track of score and display it at the end
+- Keep track of score and display it at the end
 
 
 ## Data structure
-gameArea ()
-Element ()
+- hidePlayAgainBtn() {}
+- shuffleCards(array) {}
+- createAppendDiv() {}
+- displayCategory () {}
+- attachClickEvent () {}
+- checkGameWon () {}
+- displayGameWon () {}
+- displayGameOver () {}
+- playAgain () {}
+- startCountdown () {}
 
 
 ## States y States Transitions
-_List of states (views) of your game_
+- Start game
+- Game (w board area)
+- Game over/game lost
+- Play again
 
 
 ## Task
-_List of tasks in order of priority_
+- HTML
+- createAppendDiv() {}
+- displayCategory () {}
+- attachClickEvent () {}
+- checkGameWon () {}
+- displayGameWon () {}
+- displayGameOver () {}
+- playAgain () {}
+- startCountdown () {}
+- shuffleCards(array) {}
+- hidePlayAgainBtn() {}
+- CSS
+
+
 
 
 ## Links
 
-- [Trello Link](https://trello.com)
-- [Slides Link](http://slides.com)
-- [Github repository Link](http://github.com)
-- [Deployment Link](http://github.com)
+- [Trello Link](https://trello.com/b/IR0iFwvK/module-1-project-chihuahua-or-muffin)
+- [Slides Link](https://docs.google.com/presentation/d/1zcWcIphPKtOGlQlbupQ7GoAbKnYqc2_nx650Tzl4IQc/edit?usp=sharing)
+- [Github repository Link](https://github.com/giulia-colombo/chihuahua-or-muffin)
+- [Deployment Link](https://giulia-colombo.github.io/chihuahua-or-muffin/)
--- script/index.js
+++ script/index.js
@@ -1,59 +1,9 @@
-//TO DO:
-//1. when clicking start button
-//start with a clear game area
-//1.a display imgs - done
-//1.b display a random category - done
-//1.c start timer   
-    //NOT WORKING PROPERLY  - not starting exactly from 0
-
-//2. when img clicked
-// nb the array that contains the cards could be shuffled everytime the play button is played
-//2.a check category, display right or wrong message - THIS NOT WORKING WELL
-//only works on game restart, not on game play
-
-//2.b shade img - done
-//2.c if category right, display category - done
-//2.d if all cards clicked - game finished - done
-//2.e if category wrong,  game finished/over // THIS NOT WORKING PROP
-
-
-//4. if time runs out
-//game game finished/over - DONE
-
-//3a. if game finished/won, 
-//stop timer
-//hide everything
-//display play again button
-
-//4a. if game finished/over, 
-//display play again page w button
-
-//5a. if play again,
-//restart game board by either
-// a) generating a new board using the class, or
-// b) clearing the board 
-
-//different cases are
-//1 - we click on all the right images: game won - needs work
-//2 - we click on one wrong image: game lost - WORKS, but not showing play again
-//3 - time runs out: game lost - WORKS, but not showing play again
-//4 - we don't click on all the right images in time - game lost
-
-//THINGS STILL NOT WORKING PROPERLY:
-//if we click on the correct card, and the on a wrong card, we don't get the msg "Sure that was chihuahua/muffin?" - fixed, I think
-//timer is not working properly when we Play Again - doesn't start from 0
-//play again should not display the game board - fixed, I think
-// "oh oh time is out" is stil displaying when we relaunch the game - fixed, I think
-//start screen shouldn't display game board on load - FIXED, I THINK
-
-
 
 const screenPlay = document.querySelector("#screen-play");
 const gameBoard = document.querySelector("#game-board"); //our parent
 const startBtn = document.querySelector("#start-btn");
 const playAgainBtn = document.querySelector("#play-again-btn");
 const cards = document.getElementsByClassName("card") //array of all the div with class card
-// console.log(cards);
 const displayedCategory = document.querySelector("#displayed-category");
 const message = document.querySelector("#message");
 const gameStatusMessage = document.querySelector("#game-status-message");
@@ -87,8 +37,6 @@ const imagesChiMuf = [
 class gameArea {
     constructor(images, width, height, category, cardsClicked, gameFinished) {
         this.images = [];
-        this.width = "300px"
-        this.height = "300px";
         this.category = "";
         this.cardsClicked = [];
         this.timer = 7;
@@ -209,15 +157,13 @@ class gameArea {
         },2000)
         
         gameStatusMessage.style.display = "block";
-        gameStatusMessage.innerHTML = "Game over!" //too fast
+        gameStatusMessage.innerHTML = "Game over!" 
         this.playAgain();
         
     }
 
 
-    playAgain () { //it's getting called 2x
-        // what happens when game is finished (this.gameFinished === true), and we ask if player wants to play again
-            //make the game-board go away    
+    playAgain () {    
             this.images = [];
             this.cardsClicked = [];
             this.timer = 7;
@@ -233,9 +179,6 @@ class gameArea {
                 startBtn.innerHTML = "Play again";
             }, 2000)
 
-            console.log("complete");
-            //create button to restart/play again
-            //display "Play again button?"
     }
 
 
@@ -279,7 +222,6 @@ startBtn.addEventListener('click', (event) => {
 
 
 
-//playAgain is being called multiple times the first time we play again. (console.logs x 2)
 
 
 
